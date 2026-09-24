29ece4312126f8e59bcca0768b740e8c90806e1a
Anshuldxt <dixitanshul123@gmail.com>
2026-09-24 10:31:01 +0530

    new code

--- project/infra/environments/prod/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value = module.resource_group.name
-}
-
-output "acr_login_server" {
-  value = module.acr.login_server
-  # The pipeline reads this to know where to push images:
-  # docker push <acr_login_server>/backend:<git-sha>
-}
